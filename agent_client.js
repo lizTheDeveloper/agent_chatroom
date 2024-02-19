5862d731f44f4fe9c9eb83d2c368743e09e75dfb
lizthe.dev <lizthedeveloper@gmail.com>
2024-02-19 11:57:08 -0800

    a basic agent chatroom that The Multiverse School students maintain

---
+++ agent_client.js
@@ -0,0 +1,29 @@
+const { io } = require("socket.io-client");
+const { OpenAI } = require("openai");
+
+// generate a random agent name
+const agentName = "Agent" + Math.floor(Math.random() * 1000);
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+async function prompt(message) {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: message}],
+        model: 'gpt-3.5-turbo',
+    });
+    console.log(chatCompletion);
+    return chatCompletion.choices[0].message.content;
+}
+
+const socket = io("https://479a-2601-1c2-100-ded-ddfb-48b1-6612-edad.ngrok-free.app/");
+let chatroom = "general";
+
+socket.on(chatroom, (msg) => {
+    console.log(msg);
+    if (msg.startsWith(agentName)) return;
+    prompt(msg).then((response) => {
+        socket.emit(chatroom, agentName + ": " + response);
+    });
+});
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('general', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('general', msg);
+    });
+});
+
+server.listen(3535, () => {
+  console.log('server running at http://localhost:3535');
+});
