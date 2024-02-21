2a10abdaf4676c749b3958ee776885d3b9596d10
lizthe.dev <lizthedeveloper@gmail.com>
2024-02-21 11:18:40 -0800

    added user login and registration using in-memory sessions, needs fixing but does meet acceptance criteria

--- index.js
+++ index.js
@@ -7,18 +7,58 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+let users = {}; //not a real backend, just for testing
+
+
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    if (!socket.username) {
+        socket.emit('general', 'Please register or login with /register <username> <password> or /login <username> <password>');
+    }
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     socket.on('general', (msg) => {
         console.log('message: ' + msg);
-        io.emit('general', msg);
+
+        // handle slash commands
+        if (msg.startsWith('/')) {
+            const command = msg.split(' ')[0];
+            const args = msg.split(' ').slice(1);
+            switch (command) {
+                case '/register':
+                    if (users[args[0]]) {
+                        socket.emit('general', 'Username already taken');
+                        return;
+                    } else {
+                        socket.emit('general', 'Registered');
+                        users[args[0]] = args[1];
+                    }
+                    break;
+                case '/login':
+                    // check the password
+                    if (users[args[0]] === args[1]) {
+                        socket.emit('general', 'Logged in');
+                        socket.username = args[0];
+                    }
+                    break;
+                default:
+                    socket.emit('general', 'Unknown command');
+            }
+            return;
+        } else {
+            if (!socket.username) {
+                socket.emit('general', 'Please register or login with /register <username> <password> or /login <username> <password>');
+                return;
+            }
+            io.emit('general', socket.username + " " + msg);
+        }
+
+
     });
 });
 
