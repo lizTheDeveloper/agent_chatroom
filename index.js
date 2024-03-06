74995dea5ef9f282a3437b06dc415c2964f56421
lizthe.dev <lizthedeveloper@gmail.com>
2024-03-06 11:48:53 -0800

    added secure server if env is production and fixed url

--- index.js
+++ index.js
@@ -258,6 +258,6 @@ io.on('connection', (socket) => {
     }
 });
 
-server.listen(3535, () => {
-  console.log('server running at http://localhost:3535/');
+server.listen(80, () => {
+  console.log('server running at http://localhost:80/');
 });
