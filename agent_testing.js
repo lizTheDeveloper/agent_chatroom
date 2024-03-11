e8a5cad241de700f28d0ca3778e7b50cedae9224
lizthe.dev <lizthedeveloper@gmail.com>
2024-03-11 11:38:22 -0700

    interstitial commit

---
+++ agent_testing.js
@@ -0,0 +1,14 @@
+let { Agent } = require('./agents/AddressableAgent.js');
+
+let agent = new Agent("agentchatbot", "hi");
+agent.connect();
+agent.registerMessageHandler("general", (msg) => {
+    console.log(msg, msg.includes("@" + this.agentName));
+    if (msg.startsWith("agentchatbot")) return;
+    if (msg.includes("@" + this.agentName)) {
+        console.log(msg)
+        agent.prompt(msg).then((response) => {
+            agent.sendMessage("general", response);
+        });
+    }
+});
