6fee316eb0a0032a433e37f432ec720d337b51b9
lizthe.dev <lizthedeveloper@gmail.com>
2024-03-11 11:45:42 -0700

    abstract agent

--- agent_testing.js
+++ agent_testing.js
@@ -2,6 +2,8 @@ let { Agent } = require('./agents/AddressableAgent.js');
 
 let agent = new Agent("agentchatbot", "hi");
 agent.connect();
+
+
 agent.registerMessageHandler("general", (msg) => {
     console.log(msg, msg.includes("@" + this.agentName));
     if (msg.startsWith("agentchatbot")) return;
---
+++ agents/AddressableAgent.js
@@ -0,0 +1,41 @@
+const { io } = require("socket.io-client");
+const { OpenAI } = require("openai");
+
+class Agent {
+    constructor(agentName, password, chatroom="general", socketUrl="https://chat.themultiverse.school:3535") {
+        this.agentName = agentName;
+        this.password = password;
+        this.chatroom = chatroom;
+        // Clients
+        this.openai = new OpenAI({
+            apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+        });
+
+        this.socketUrl = socketUrl;
+    }
+
+    connect() {
+        this.socket = io(this.socketUrl);
+        this.socket.emit(this.chatroom, "/login " + this.agentName + " " + this.password);
+    }
+
+    registerMessageHandler(channel, handler) {
+        this.socket.on(channel, handler.bind(this));
+    }
+
+    prompt(message) {
+        return this.openai.chat.completions.create({
+            messages: [{ role: 'user', content: message}],
+            model: 'gpt-3.5-turbo',
+        }).then((chatCompletion) => {
+            return chatCompletion.choices[0].message.content;
+        });
+    }
+
+    sendMessage(channel, message) {
+        this.socket.emit(channel, this.agentName + ": " + message);
+    }
+}
+
+module.exports = { Agent }
+
